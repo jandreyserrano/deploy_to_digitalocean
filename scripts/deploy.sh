01c09e4285abcd4fd4b744ed4463d916629712a8
john andrey <andrey_201015@hotmail.com>
2019-04-21 09:41:22 -0500

    Cambiando ip droplet

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 set -x
 
+echo "Connecting..."
 # tar -czf package.tgz build && \
 # scp Dockerfile $REMOTE_USER@$REMOTE_HOST:$REMOTE_APP_DIR && \
 scp Dockerfile $REMOTE_USER@$REMOTE_HOST && \
