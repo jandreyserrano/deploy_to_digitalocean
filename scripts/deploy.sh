bcdd16793267e942926582068ccc42ac08452370
john andrey <andrey_201015@hotmail.com>
2019-04-21 10:07:32 -0500

    Cambiando travis config

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,9 +1,24 @@
-#!/usr/bin/env sh
-set -x
+# #!/usr/bin/env sh
+# set -x
 
-echo "Connecting..."
-echo $REMOTE_USER
+# echo "Connecting..."
+# echo $REMOTE_USER
 # tar -czf package.tgz build && \
 # scp Dockerfile $REMOTE_USER@$REMOTE_HOST:$REMOTE_APP_DIR && \
-scp Dockerfile $REMOTE_USER@$REMOTE_HOST && \
-ssh -y $REMOTE_USER@$REMOTE_HOST 'bash -s' < ./scripts/untar.sh
+# scp Dockerfile $REMOTE_USER@$REMOTE_HOST && \
+# ssh -y $REMOTE_USER@$REMOTE_HOST 'bash -s' < ./scripts/untar.sh
+
+
+#!/bin/bash
+set -xe
+
+if [ $TRAVIS_BRANCH == 'master' ] ; then
+  eval "$(ssh-agent -s)"
+  ssh-add ~/.ssh/id_rsa
+
+  echo "Connecting..."
+  echo $REMOTE_USER
+  ls
+else
+  echo "Not deploying, since this branch isn't master."
+fi
