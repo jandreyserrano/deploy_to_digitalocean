ffd19274732140a7af8edb7f7fd5f71deffa211f
john andrey <andrey_201015@hotmail.com>
2019-04-21 16:34:57 -0500

    Changes config docker deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -10,11 +10,14 @@ if [ $TRAVIS_BRANCH == 'master' ] ; then
   docker push jandrey15/nginx:latest
   
   echo "----------------"
+  
+  echo export TRAVIS_BUILD_NUMBER=$TRAVIS_BUILD_NUMBER > .env_travis
 
   eval "$(ssh-agent -s)"
   ssh-add ~/.ssh/id_rsa
   echo "Connecting..."
   scp -o StrictHostKeyChecking=no Dockerfile $REMOTE_USER@$REMOTE_HOST:~/test && \
+  scp -o StrictHostKeyChecking=no .env_travis $REMOTE_USER@$REMOTE_HOST:~/ && \
   ssh -o StrictHostKeyChecking=no $REMOTE_USER@$REMOTE_HOST 'bash -s' < ./scripts/untar.sh
   echo "Deployment..."
 
--- scripts/untar.sh
+++ scripts/untar.sh
@@ -2,13 +2,18 @@
 set -x
 
 # echo "Travis build  number - ${TRAVIS_BUILD_NUMBER}"
+pwd && \
+source .env_travis && \
+cd test/ && \
 
-cd test/
-
-echo "-------------------"
+echo "-------------------" && \
 docker ps
 # docker rmi $(docker images -q)
-docker pull jandrey15/nginx:latest
+# docker pull jandrey15/nginx:latest
+docker pull jandrey15/nginx:$TRAVIS_BUILD_NUMBER
+# docker run --name my-nginx-$TRAVIS_BUILD_NUMBER -d -p 80:80 jandrey15/nginx:latest
+docker stop my-nginx
+docker run --rm --name my-nginx -d -p 80:80 jandrey15/nginx:$TRAVIS_BUILD_NUMBER
 # docker build -t jandrey15/nginx:$TRAVIS_BUILD_NUMBER .
 # docker run --name my-nginx -d -p 8080:80 jandrey15/nginx
 docker ps -a
