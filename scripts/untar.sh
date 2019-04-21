2033f5593240f2210b01f5bad571fb7fdd0eb8da
john andrey <andrey_201015@hotmail.com>
2019-04-21 12:18:39 -0500

    Changes config docker deploy

--- scripts/untar.sh
+++ scripts/untar.sh
@@ -1,16 +1,15 @@
 #!/usr/bin/env sh
 set -x
 
-echo "Travis build  number - ${TRAVIS_BUILD_NUMBER}"
+# echo "Travis build  number - ${TRAVIS_BUILD_NUMBER}"
 
 cd test/
 
 echo "-------------------"
 docker ps
-docker rmi $(docker images -q)
-docker pull jandrey15/nginx
+# docker rmi $(docker images -q)
+docker pull jandrey15/nginx:latest
 # docker build -t jandrey15/nginx:$TRAVIS_BUILD_NUMBER .
 # docker run --name my-nginx -d -p 8080:80 jandrey15/nginx
 docker ps -a
-
-docker rm $(docker ps -a -q)
+# docker rm $(docker ps -a -q)
