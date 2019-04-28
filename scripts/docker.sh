78ac01148f7f9500a7e1ab553415601a49b8c053
john andrey <andrey_201015@hotmail.com>
2019-04-28 12:31:43 -0500

    Chnages html y script deploy

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env sh
 set -x
 
-# echo "Travis build  number - ${TRAVIS_BUILD_NUMBER}"
 pwd && \
 source .env_travis && \
 # cd test/ && \
 
 echo "-------------------" && \
+
+echo $TRAVIS_BUILD_NUMBER
 docker service ls
-docker service update --image jandrey15/nginx my-nginx
+docker service update --image jandrey15/nginx:$TRAVIS_BUILD_NUMBER my-nginx
 docker service ps my-nginx
